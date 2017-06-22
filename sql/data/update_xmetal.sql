d7dd16f115db2b0711d84e0a5d14a867cfd6e32b
Stephen Kew <stephen.kew@gmail.com>
2017-06-22 14:06:47 +0200

    Added script to update the penny emerald

---
+++ sql/data/update_xmetal.sql
@@ -0,0 +1,2 @@
+UPDATE model SET image='http://www.aquaria.za.net/oakleydb/images/X-Metal/Penny_TiO2_Emerald.jpg', imagesmall='http://www.aquaria.za.net/oakleydb/images/X-Metal/small/Penny_TiO2_Emerald.jpg' WHERE sku='04-133'
+
